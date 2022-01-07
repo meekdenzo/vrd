20c35200c8c06a9b9308dd086c8a6cf6d5d12a23
Parallels <meekdenzo@gmail.com>
2022-01-07 16:21:33 -0600

    improve: entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,21 +7,20 @@ echo "Checking/installing dependencies..."
 command -v curl || sudo apt-get install -yq curl
 command -v jq || sudo apt-get install -yq jq
 
-CHANGED_FILES=$(git diff-tree --no-commit-id -r $GITHUB_SHA | grep -v "^src/validate*")
-echo "$CHANGED_FILES"
+CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep -v "^src/validate*")
+echo "The following files were changed: \n$CHANGED_FILES"
 
 # Run configurations
-git clone https://github.com/davisjam/vuln-regex-detector.git
-cd vuln-regex-detector
+STABLE_RELEASE="54ddfd60ced5ea0735ed42b910505fa14d3b41bf"
+CLONE_DIR="vuln-regex-detector"
+
+git clone https://github.com/davisjam/vuln-regex-detector.git $CLONE_DIR
+cd $CLONE_DIR
+git checkout $STABLE_RELEASE
 VULN_REGEX_DETECTOR_ROOT=$(pwd)
 export VULN_REGEX_DETECTOR_ROOT
 ./configure
 cd ..
-
-# test
-echo '{"file":"./autoInject.js"}' > checkfile.json   
-perl ./bin/check-file.pl checkfile.json > checkfile-out.json
-jq -r '.vulnRegexes | .[]?' < checkfile-out.json
 
 # Scan for redos
 SECONDS=0
