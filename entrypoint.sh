24b040dfe4a38a0a5e8a62b4f3f3b26940617d5a
Meek Denzo <meekdenzo@gmail.com>
2023-10-09 00:08:43 -0400

    test: add and test `regexploit`

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,22 +2,25 @@
 
 # Install dependencies
 echo "Checking/installing dependencies..."
-command -v curl || sudo apt-get install -yq curl
-command -v jq || sudo apt-get install -yq jq
+
 
 CHANGED_FILES=$(git diff --name-only HEAD^ HEAD | grep -v "^src/validate*")
 echo "The following files were changed: \n$CHANGED_FILES"
 
 # Run configurations
-STABLE_RELEASE="54ddfd60ced5ea0735ed42b910505fa14d3b41bf"
-CLONE_DIR="vuln-regex-detector"
+STABLE_RELEASE="95cec2d81c50722abbfcd46957ab184c3c767cf3"
+CLONE_DIR="regexploit"
 
-git clone https://github.com/davisjam/vuln-regex-detector.git $CLONE_DIR
+git clone https://github.com/meekdenzo/regexploit.git $CLONE_DIR
 cd $CLONE_DIR
 git checkout $STABLE_RELEASE
 VULN_REGEX_DETECTOR_ROOT=$(pwd)
 export VULN_REGEX_DETECTOR_ROOT
-./configure
+
+python3 -m venv .env
+source .env/bin/activate
+pip install regexploit
+
 cd ..
 
 # Scan for redos
@@ -25,25 +28,27 @@ VULN_COUNT=0
 SECONDS=0
 for i in ${CHANGED_FILES}
     do
-        echo "Scanning for vulnerable regexes in $i"
-        echo '{"file":"'"$i"'"}' > checkfile.json
+        # echo "Scanning for vulnerable regexes in $i"
+        # echo '{"file":"'"$i"'"}' > checkfile.json
        
-        perl ./$CLONE_DIR/bin/check-file.pl checkfile.json > checkfile-out.json
+        # perl ./$CLONE_DIR/bin/check-file.pl checkfile.json > checkfile-out.json
 
-        echo "The following vulnerable regexes were found in $i"
-        jq -r '.vulnRegexes | .[]?' < checkfile-out.json
-        printf "\n\n\n\n"
+        # echo "The following vulnerable regexes were found in $i"
+        # jq -r '.vulnRegexes | .[]?' < checkfile-out.json
+        # printf "\n\n\n\n"
 
-        COUNT=$(jq '.anyVulnRegexes' < checkfile-out.json)
-        VULN_COUNT=$((VULN_COUNT+$COUNT))
+        # COUNT=$(jq '.anyVulnRegexes' < checkfile-out.json)
+        # VULN_COUNT=$((VULN_COUNT+$COUNT))
+
+        cat $i | regexploit
 
     done
 duration=$SECONDS;
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds elapsed for scan."
 
-if [ $VULN_COUNT -eq 0 ]; then
-    echo "No vulnerable regex was detected. You're good to go!"
-else
-    echo "$VULN_COUNT vulnerable regex(es) were detected. See logs above."
-    exit 10
-fi
+# if [ $VULN_COUNT -eq 0 ]; then
+#     echo "No vulnerable regex was detected. You're good to go!"
+# else
+#     echo "$VULN_COUNT vulnerable regex(es) were detected. See logs above."
+#     exit 10
+# fi
